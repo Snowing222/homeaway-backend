c778a5763adf613f6f91dfd70df955ee15c0a4cb
Snowing222 <snowing222@gmail.com>
2021-03-08 23:14:00 -0500

    switch to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_01_31_221743) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "listings", force: :cascade do |t|
     t.text "title"
     t.integer "property_id"
